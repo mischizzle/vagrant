31830b3e288ce098050472a6e2403982a6f152f7
Michelle Anderson <mimi@Michelles-MacBook-Pro.local>
2014-11-05 12:56:34 +0000

    Adding provisionning for homebrew install, then apache and mysql

---
+++ provision.sh
@@ -0,0 +1,11 @@
+apt-get -y update
+
+apt-get install curl
+
+apt-get install apache2
+
+apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#service mysql start
