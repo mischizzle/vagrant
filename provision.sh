43381c9439d21ef2655ef0e13dc9b5f29c502695
Michelle Anderson <michelle@picot.ca>
2014-11-06 11:35:34 +0000

    Adding gitignore file; updated the provisioning shell script

--- provision.sh
+++ provision.sh
@@ -1,11 +1,41 @@
-apt-get -y update
+#!/usr/bin/env bash
 
-apt-get install curl
+#Creating Vagrant box for web dev
 
-apt-get install apache2
+apt-get update
 
-apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql
+#install curl
+#apt-get -y install curl
 
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#install homebrew
+#ruby -e "$(wget -O- https://raw.github.com/Homebrew/linuxbrew/go/install)"
+#ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+
+#install apache2
+apt-get install -y apache2
+#rm -rf /var/www
+#ln -fs /vagrant /var/www
+
+
+#install mysql
+#apt-get install mysql-server libapache2-mod-auth-mysql php5-mysql
+#apt-get install -y php5-mysql mysql-server
+
+
+
+#install git
+
+#install nodejs, npm
+
+#install compass
+
+#install varnish3
+
+#install sequelpro
+
+#install grunt-cli
+
+#install karma
 
 #service mysql start
